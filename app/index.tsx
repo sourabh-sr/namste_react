012d83b5f9cf5d5af5bb0fa35961e666f95e1b76
Sourabh Srivatsa S R <sourabhsrivatsasr@Sourabhs-MacBook-Air.local>
2024-09-17 19:03:31 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View
+            style={{
+                flex: 1,
+            }}
+        >
+            <Text>Hello</Text>
+        </View>
+    );
+}
